6d3ed74395d56cba75b2e33818a9ee8ea5b9f164
Nikola Krsmanovic <nikolakrsmanovic342@gmail.com>
2023-06-02 16:25:01 +0200

    first commit

---
+++ java/custom.js
@@ -0,0 +1,129 @@
+var srbija=document.querySelector("#srb");
+var cg=document.querySelector("#mne");
+var bih=document.querySelector("#bih");
+var hrvatska=document.querySelector("#cro");
+var madjarska=document.querySelector("#hun");
+var rumunija=document.querySelector("#rum");
+var bugarska=document.querySelector("#bul");
+var makedonija=document.querySelector("#nmk");
+var albanija=document.querySelector("#alb");
+function Srbija(){
+    srbija.play();
+    cg.pause();
+    bih.pause();
+    hrvatska.pause();
+    madjarska.pause();
+    rumunija.pause();
+    bugarska.pause();
+    makedonija.pause();
+    albanija.pause();
+}
+
+function CrnaGora(){
+    srbija.pause();
+    cg.play();
+    bih.pause();
+    hrvatska.pause();
+    madjarska.pause();
+    rumunija.pause();
+    bugarska.pause();
+    makedonija.pause();
+    albanija.pause();
+}
+
+function Bih(){
+    srbija.pause();
+    cg.pause();
+    bih.play();
+    hrvatska.pause();
+    madjarska.pause();
+    rumunija.pause();
+    bugarska.pause();
+    makedonija.pause();
+    albanija.pause();
+}
+
+function Hrvatska(){
+    srbija.pause();
+    cg.pause();
+    bih.pause();
+    hrvatska.play();
+    madjarska.pause();
+    rumunija.pause();
+    bugarska.pause();
+    makedonija.pause();
+    albanija.pause();
+}
+
+function Madjarska(){
+    srbija.pause();
+    cg.pause();
+    bih.pause();
+    hrvatska.pause();
+    madjarska.pause();
+    rumunija.play();
+    bugarska.pause();
+    makedonija.pause();
+    albanija.pause();
+}
+
+function Rumunija(){
+    srbija.pause();
+    cg.pause();
+    bih.pause();
+    hrvatska.pause();
+    madjarska.pause();
+    rumunija.play();
+    bugarska.pause();
+    makedonija.pause();
+    albanija.pause();
+}
+
+function Bugarska(){
+    srbija.pause();
+    cg.pause();
+    bih.pause();
+    hrvatska.pause();
+    madjarska.pause();
+    rumunija.pause();
+    bugarska.play();
+    makedonija.pause();
+    albanija.pause();
+}
+
+function Makedonija(){
+    srbija.pause();
+    cg.pause();
+    bih.pause();
+    hrvatska.pause();
+    madjarska.pause();
+    rumunija.pause();
+    bugarska.pause();
+    makedonija.play();
+    albanija.pause();
+}
+
+function Albanija(){
+    srbija.pause();
+    cg.pause();
+    bih.pause();
+    hrvatska.pause();
+    madjarska.pause();
+    rumunija.pause();
+    bugarska.pause();
+    makedonija.pause();
+    albanija.play();
+}
+
+function Stop()
+{
+    srbija.pause();
+    cg.pause();
+    bih.pause();
+    hrvatska.pause();
+    madjarska.pause();
+    rumunija.pause();
+    bugarska.pause();
+    makedonija.pause();
+    albanija.pause();
+}
